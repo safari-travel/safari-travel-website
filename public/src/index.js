c20c6526de7f4180e0577572825d461e77a2ff96
safari-travel <safaritravelhargeisa@gmail.com>
2026-08-19 22:10:11 +0300

    Initialize React application with SafariWebsite component

---
+++ public/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SafariWebsite from './safari_website';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SafariWebsite />
+  </React.StrictMode>
+);
